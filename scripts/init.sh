6ce77c29fd574f314bce7e325072a0c54b8f1201
Roger <88864000+absanr@users.noreply.github.com>
2024-12-14 12:12:47 -0500

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -37,7 +37,9 @@ sed -i '/redis/d' ./Procfile
 bench new-site dev.localhost \
 --mariadb-root-password 123 \
 --admin-password admin \
---no-mariadb-socket
+# --no-mariadb-socket
+--mariadb-user-host-login-scope='%'
+
 
 bench --site dev.localhost set-config developer_mode 1
 bench --site dev.localhost clear-cache
